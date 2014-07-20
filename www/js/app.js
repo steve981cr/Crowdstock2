18067f92b8fd30b7769f0b92455f59d7b0ef15fc
Sam Pepose <sam.pepose@oasisdigital.com>
2014-07-19 18:36:19 -0700

    Progress on static screens

--- www/js/app.js
+++ www/js/app.js
@@ -1,5 +1,13 @@
 // Ionic Starter App
 
+window.addEventListener("load", function () {
+    // Set a timeout...
+    setTimeout(function () {
+        // Hide the address bar!
+        window.scrollTo(0, 1);
+    }, 0);
+});
+
 // angular.module is a global place for creating, registering and retrieving Angular modules
 // 'starter' is the name of this angular module example (also set in a <body> attribute in index.html)
 // the 2nd parameter is an array of 'requires'
@@ -7,63 +15,21 @@
 // 'starter.controllers' is found in controllers.js
 angular.module('starter', ['ionic', 'starter.services', 'starter.controllers'])
 
+    .config(function ($stateProvider, $urlRouterProvider) {
 
-.config(function($stateProvider, $urlRouterProvider) {
+        // Ionic uses AngularUI Router which uses the concept of states
+        // Learn more here: https://github.com/angular-ui/ui-router
+        // Set up the various states which the app can be in.
+        // Each state's controller can be found in controllers.js
+        $stateProvider
+            .state('info', {
+                url: '/info',
+                templateUrl: 'js/info/info-tmplt.html',
+                controller: 'InfoCtrl'
+            });
 
-  // Ionic uses AngularUI Router which uses the concept of states
-  // Learn more here: https://github.com/angular-ui/ui-router
-  // Set up the various states which the app can be in.
-  // Each state's controller can be found in controllers.js
-  $stateProvider
+        // if none of the above states are matched, use this as the fallback
+        $urlRouterProvider.otherwise('/info');
 
-    // setup an abstract state for the tabs directive
-    .state('tab', {
-      url: "/tab",
-      abstract: true,
-      templateUrl: "templates/tabs.html"
-    })
-
-    // the pet tab has its own child nav-view and history
-    .state('tab.pet-index', {
-      url: '/pets',
-      views: {
-        'pets-tab': {
-          templateUrl: 'templates/pet-index.html',
-          controller: 'PetIndexCtrl'
-        }
-      }
-    })
-
-    .state('tab.pet-detail', {
-      url: '/pet/:petId',
-      views: {
-        'pets-tab': {
-          templateUrl: 'templates/pet-detail.html',
-          controller: 'PetDetailCtrl'
-        }
-      }
-    })
-
-    .state('tab.adopt', {
-      url: '/adopt',
-      views: {
-        'adopt-tab': {
-          templateUrl: 'templates/adopt.html'
-        }
-      }
-    })
-
-    .state('tab.about', {
-      url: '/about',
-      views: {
-        'about-tab': {
-          templateUrl: 'templates/about.html'
-        }
-      }
     });
-
-  // if none of the above states are matched, use this as the fallback
-  $urlRouterProvider.otherwise('/tab/pets');
-
-});
 
--- www/js/controllers.js
+++ www/js/controllers.js
@@ -2,14 +2,27 @@ angular.module('starter.controllers', [])
 
 
 // A simple controller that fetches a list of data from a service
-.controller('PetIndexCtrl', function($scope, PetService) {
+.controller('InfoCtrl', function($scope, PetService) {
   // "Pets" is a service returning mock data (services.js)
   $scope.pets = PetService.all();
 })
+    .directive('graph', function () {
+        return {
+            restrict : "EA",
+            template : "<div></div>",
+            link : function() {
+                var d1 = [[1, 300], [2, 600], [3, 550], [4, 400], [5, 300]];
+                $(document).ready(function () {
+                    $.plot($(".graph"), [d1]);
+                });
+                $(window).resize(function() { $.plot($(".graph"), [d1]);});
+            }
+        }
+    })
 
 
 // A simple controller that shows a tapped item's data
-.controller('PetDetailCtrl', function($scope, $stateParams, PetService) {
+.controller('PlayCtrl', function($scope, $stateParams, PetService) {
   // "Pets" is a service returning mock data (services.js)
   $scope.pet = PetService.get($stateParams.petId);
 });
